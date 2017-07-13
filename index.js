1f41cb514091062b41faf1b7e15d5ab368abb933
ghjpharvey <ghjpharvey@gmail.com>
2017-07-13 16:19:56 -0400

    changes date-time seperator to 'T' as is convention

--- index.js
+++ index.js
@@ -179,19 +179,19 @@ function companyInfoTemplate (data, parameter) {
 }
 function launchInfoTemplate (data, parameter, past) {
   let tense = past?  "took place":"is due to take place";
-  let date = new Date(data.launch_date + ',' + data.time_utc);
+  let date = new Date(data.launch_date + 'T' + data.time_utc);
   let dateString0 = date.toUTCString();
   const LAUNCH_INFO = {
     "flight_number": `${data.flight_number}.`,
     "launch_year": `${data.launch_year}.`,
     "launch_date": `The launch of ${data.payload_1} aboard SpaceX's ${data.rocket} from ${PAD_ID_NAME[data.launch_site]} ${tense} at ${dateString0}. `,
-    "time_utc": `${data.time_utc}`,
+    "time_utc": `${dateString0}`,
     "time_local": `${data.time_local}`,
     "rocket": `${data.rocket}`,
     "rocket_type": `${data.type}`,
     "core_serial": `${data.core_serial}`,
     "cap_serial": `${data.cap_serial}`,
-    "launch_site": `${PAD_ID_NAME[data.launch_site]}`,
+    "launch_site": `${PAD_ID_NAME[data.launch_site]}`,//could consider adding a google maps rich data reply
     "payload_1": `${data.payload_1}`,
     "payload_2": `${data.payload_2}`,
     "payload_type": `${data.payload_type}`,
