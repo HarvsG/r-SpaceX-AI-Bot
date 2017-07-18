9c2891d26453f1ec533a57f0a96f82c54ecc9980
ghjpharvey <ghjpharvey@gmail.com>
2017-07-18 15:45:00 -0400

    vehicle info dict

--- index.js
+++ index.js
@@ -55,106 +55,45 @@ const PAD_ID_NAME = {//This translation should probably happen via the API for f
 };
 function vehicleInfoTemplate (data, parameter) {
   const VEHICLE_INFO = {
-    "id": "falcon9",
-    "name": "Falcon 9",
-    "active": true,
-    "stages": 2,
-    "cost_per_launch": 62000000,
-    "success_rate_pct": 94,
-    "first_flight": "2010-06-04",
-    "country": "United States",
-    "company": "SpaceX",
-    "height": {
-      "meters": 70,
-      "feet": 229.6
-    },
-    "diameter": {
-      "meters": 3.7,
-      "feet": 12
-    },
-    "mass": {
-      "kg": 549054,
-      "lb": 1207920
-    },
-    "payload_weights": [
-      {
-        "id": "leo",
-        "name": "low earth orbit",
-        "kg": 22800,
-        "lb": 50265
-      },
-      {
-        "id": "gto",
-        "name": "geosynchronous transfer orbit",
-        "kg": 8300,
-        "lb": 18300
-      },
-      {
-        "id": "mars",
-        "name": "mars orbit",
-        "kg": 4020,
-        "lb": 8860
-      }
-    ],
-    "first_stage": {
-      "reusable": true,
-      "engines": 9,
-      "fuel_amount_tons": 385,
-      "burn_time_sec": 180,
-      "thrust_sea_level": {
-        "kN": 7607,
-        "lbf": 1710000
-      },
-      "thrust_vacuum": {
-        "kN": 8227,
-        "lbf": 1849500
-      }
-    },
-    "second_stage": {
-      "engines": 1,
-      "fuel_amount_tons": 90,
-      "burn_time_sec": 397,
-      "thrust": {
-        "kN": 934,
-        "lbf": 210000
-      },
-      "payloads": {
-        "option_1": "dragon",
-        "option_2": "composite fairing",
-        "composite_fairing": {
-          "height": {
-            "meters": 13.1,
-            "feet": 43
-          },
-          "diameter": {
-            "meters": 5.2,
-            "feet": 17.1
-          }
-        }
-      }
-    },
-    "engines": {
-      "number": 9,
-      "type": "merlin",
-      "version": "1D+",
-      "layout": "octaweb",
-      "engine_loss_max": 2,
-      "propellant_1": "liquid oxygen",
-      "propellant_2": "RP-1 kerosene",
-      "thrust_sea_level": {
-        "kN": 845,
-        "lbf": 190000
-      },
-      "thrust_vacuum": {
-        "kN": 914,
-        "lbf": 205500
-      },
-      "thrust_to_weight": 180.1
-    },
-    "landing_legs": {
-      "number": 4,
-      "material": "carbon fiber"
-    },
+    "id": ``,
+    "name": ``,
+    "active": ``,
+    "stages": ``,
+    "cost_per_launch": ``,
+    "success_rate_pct": ``,
+    "first_flight": ``,
+    "country": ``,
+    "company": ``,
+    "height": ``,
+    "diameter": ``,
+    "radius":`${data.diameter.meters/2} meters`,
+    "circumfrance":`${data.diameter.meters*Math.PI} meters`,
+    "mass": `${data.mass.kg} kilograms`,
+    "leo_weight": `${data.payload_weights[0].kg} kilograms to ${data.payload_weights[0].name}`,
+    "gto_weight": `${data.payload_weights[1].kg} kilograms to ${data.payload_weights[1].name}`,
+    "mars_weight": `${data.payload_weights[2].kg} kilograms to ${data.payload_weights[2].name}`,
+    "first_stage_reusable": `${data.first_stage.reusable}`,
+    "first_stage_engine_count":`The first stage has ${data.first_stage.engines} engines`,
+    "first_stage_fuel_amount":`${data.first_stage.fuel_amount_tons}`,
+    "first_stage_burn_time":`${data.first_stage.burn_time_sec}`,
+    "first_stage_thrust_sea_level":`${data.first_stage.thrust_sea_level.kN} kilonewtons`,
+    "first_stage_thrust_sea_vacuum":`${data.first_stage.thrust_sea_vaccuum.kN} kilonewtons`,
+    "second_stage_engine_count":`The first stage has ${data.second_stage.engines} engines`,
+    "second_stage_fuel_amount":`${data.second_stage.fuel_amount_tons}`,
+    "second_stage_burn_time":`${data.second_stage.burn_time_sec}`,
+    "second_stage_thrust_sea_level":`${data.second_stage.thrust.kN} kilonewtons`,
+    "second_stage_payload_options":`${data.second_stage.payloads.option_1} and ${data.second_stage.payloads.option_2}`,
+    "fairing_height":` ${data.second_stage.payloads.composite_fairing.height.meters} meters`,
+    "fairing_diameter":` ${data.second_stage.payloads.composite_fairing.diameter.meters} meters`,
+    "engine_type":`${data.engines.type} version ${data.engines.version}`,
+    "engine_layout":`${data.engines.layout}`,
+    "engine_loss_max":`${data.engines.engine_loss_max} can be loss during flight without causing mission failure`,
+    "propellants":`${data.engines.propellant_1} and ${data.engines.propellant_2}`,
+    "thrust_sea_level":`${data.engines.thrust_sea_level.kN} kilonewtons`,
+    "thrust_vaccuum":`${data.engines.thrust_sea_vaccuum.kN} kilonewtons`,
+    "thrust_to_weight":`${data.engines.thrust_to_weight} thrust to weight ratio`,
+    "landing_legs_material":`The landing legs on the ${data.name} are made from ${data.landing_legs.material}`,
+    "landing_legs_number":`The ${data.name} has ${data.landing_legs.number} landing legs`,
     "description": "Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit."
   }
   return VEHICLE_INFO[parameter]
